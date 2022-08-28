349ecc4222730589683c53131aba66baadde38c7
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2022-08-28 18:27:34 +0500

    bubble sort

--- src/algorithms/sorting/BubbleSort.kt
+++ src/algorithms/sorting/BubbleSort.kt
@@ -1,5 +1,22 @@
 package algorithms.sorting
 
 fun main() {
-    
+    val list = arrayListOf(6,4,5,3,2,1)
+    println("Before sort: $list")
+    bubbleSort(list)
+    println("After sort: $list")
+}
+
+fun bubbleSort(numbers: ArrayList<Int>) {
+    for (pass in 0 until (numbers.size - 1)) {
+        // A single pass of bubble sort
+        for (currentPosition in 0 until (numbers.size - 1)) {
+            // This is a single step
+            if (numbers[currentPosition] > numbers[currentPosition + 1]) {
+                val tmp = numbers[currentPosition]
+                numbers[currentPosition] = numbers[currentPosition + 1]
+                numbers[currentPosition + 1] = tmp
+            }
+        }
+    }
 }

b3ecc1075e584e9eb1cfa41d0fc3f4e9f80e3e7d
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2022-08-30 22:12:30 +0500

    Quick sort

--- src/algorithms/sorting/QuickSort.kt
+++ src/algorithms/sorting/QuickSort.kt
@@ -3,22 +3,22 @@ package algorithms.sorting
 fun main() {
     val list = arrayListOf(6,4,5,3,2,1)
     println("Before sort: $list")
-    val ordered =  quicksort(list)
-    println("After sort: $ordered")
+    val sortedList =  quicksort(list)
+    println("After sort: $sortedList")
 }
 fun quicksort(items:List<Int>):List<Int>{
     if (items.count() < 2){
         return items
     }
-    val pivot = items[items.count()/2]
+    val i = items[items.count()/2]
 
-    val equal = items.filter { it == pivot }
+    val equal = items.filter { it == i }
 //    println("pivot value is : "+equal)
 
-    val less = items.filter { it < pivot }
+    val less = items.filter { it < i }
 //    println("Lesser values than pivot : "+less)
 
-    val greater = items.filter { it > pivot }
+    val greater = items.filter { it > i }
 //    println("Greater values than pivot : "+greater)
 
     return quicksort(less) + equal + quicksort(greater)

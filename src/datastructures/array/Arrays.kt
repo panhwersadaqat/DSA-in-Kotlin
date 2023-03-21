3f87811e773dd69dcaf65d465e2d7d58937b99fc
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-03-21 21:52:15 +0400

    Added comments and worked on string example

--- src/datastructures/array/Arrays.kt
+++ src/datastructures/array/Arrays.kt
@@ -1,44 +1,56 @@
-package datastructures
+package datastructures.array
 
 fun main() {
     insertExample()
     appendExample()
     deleteExample()
+    lookUpExample()
 }
 
+/**
+    -Time Complexity: Linear runtime -> O(n)
+    -It move elements to right for adding new element on desire index.
+ */
 private fun insertExample() {
     val list = arrayListOf<Int>(2,4,6,10)
-
-        /*
-            -Linear runtime
-            -It move elements to right for adding new element on desire index.
-        */
 
     list.add(3,8)
 
     println(list)
 }
 
+/**
+    -Time Complexity: Constant runtime O(1)
+    - It can be O(n) in dynamic array because when you add
+      extra element in array, it copy all the items from one array
+      to new array by looping all through the first array.
+    -It does not move elements index for adding,
+     it just add new value at the end of list.
+*/
 private fun appendExample() {
     val list = arrayListOf<Int>(2,4,6,8)
-
-      /*
-          -Constant runtime
-           -It does not move elements index for adding , it just add new value at the end of list.
-     */
 
     list += 10
 
     println(list)
 }
 
+/**
+    -Time Complexity: Linear runtime O(n)
+    -It move elements to left for removing element on desire index.
+*/
 private fun deleteExample() {
     val list = arrayListOf<Int>(2,4,6,8,10,12)
-        /*
-            -Linear runtime
-            -It move elements to left for removing element on desire index.
-        */
 
     list.remove(12)
+
     println(list)
+}
+
+/**
+    -Time Complexity: Constant runtime => O(1)
+ */
+private fun lookUpExample() {
+    val list = arrayOf<Int>(2,4,6,8,10,12)
+    println(list[2])
 }
